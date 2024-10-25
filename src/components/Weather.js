1d6810be105bda8276896acc9869a26ce9e5176d
Athos Ishikawa <athos.iskw@gmail.com>
2024-10-25 14:31:55 -0300

    Created the weather component and added the css

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -6,7 +6,38 @@ const Weather = () => {
         <div className='weather'>
             <div className='search-bar'>
                 <input type='text' placeholder='Pesquise em um local'/>
-                
+
+            </div>
+
+            <div className='container'>
+                <div className = 'header'>
+                    <div className='location'>
+                        <p>DALLAS</p>
+                    </div>
+                    <div className='temperature'>
+                        <h1>60 F</h1>
+                    </div>
+
+                    <div className='description'>
+                        <p>Cloudy</p>
+                    </div>
+                </div>
+
+                <div className='footer'>
+                    <div className='sensation'>
+                        65 F
+                        <p>Sensação Térmica</p>
+                    </div>
+                    <div className='humidity'>
+                        40%
+                        <p>Humidade</p>
+                    </div>
+                    <div className='wind'>
+                        12MPH
+                        <p>Vento</p>
+                    </div>
+
+                </div>
             </div>
         </div>
     )
