4e88153f0708658e016064d1519dba2af1648e03
Athos Ishikawa <athos.iskw@gmail.com>
2024-10-28 16:23:36 -0300

    Added c2f toggle. Tested lottie animation

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,12 +1,14 @@
 import React, { Component, useState } from 'react';
 import '../styles/Toggle.css';
 
-const Toggle = () => {
+const Toggle = ({onChange}) => {
 
     const [isCelsius, setIsCelsius] = useState(true); 
 
     const handleToggle = () => {
-        setIsCelsius(!isCelsius); 
+        const newIsCelsius = !isCelsius;
+        setIsCelsius(newIsCelsius); // Update state directly
+        onChange && onChange(newIsCelsius); // Pass the updated value
     };
   
     const toggleStyle = {
@@ -16,18 +18,18 @@ const Toggle = () => {
     const selectedCelciusStyle = {
         color: isCelsius ? "#FFFFFF" : "#000000",
     };
-    const selectedFerenheitsStyle = {
+    const selectedFarenheitsStyle = {
         color: isCelsius ? "#000000" : "#FFFFFF",
     };
 
     return (
-        <div class="form-box">
+        <div className="form-box">
 
             <div className="button-box">
 
                 <div id="btn" style={toggleStyle}></div>
                 <button type="button" className="toggle-btn" style={selectedCelciusStyle} onClick={handleToggle}>°C</button>
-                <button type="button" className="toggle-btn" style={selectedFerenheitsStyle} onClick={handleToggle}>°F</button>
+                <button type="button" className="toggle-btn" style={selectedFarenheitsStyle} onClick={handleToggle}>°F</button>
 
             </div>
 
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,89 +1,107 @@
-import React, { useState } from 'react'; 
+import React, { useState, useEffect } from 'react'; 
 import '../styles/Weather.css'
 import Toggle from '../components/Toggle';
 import axios from 'axios'
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
 
-const APIKEY = 'COLOQUE SUA CHAVE API AQUI'
 
+const APIKEY = '99fbd3069d524dddcd584bc06d4e3345'
 const Weather = () => {
+    const [data, setData] = useState({});
+    const [location, setLocation] = useState(''); 
+    const [unit, setUnit] = useState('metric');
+    const [tempUnit, setTempUnit] = useState('°C');
+    const [lastSearchedLocation, setLastSearchedLocation] = useState('');
+    const [velocityUnit, setVelocityUnit] = useState('km/h');
+    const [language, setLanguage] = useState('pt_br')
 
-    const[data,setData] = useState({})
-    const[location, setLocation] = useState('') 
-    const[tempUnit, setTempUnit] = useState('°C')
-    const[velocityUnit, setVelocityUnity] = useState('km/h')
+    const fetchWeatherData = (loc) => {
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${loc}&units=${unit}&lang=${language}&appid=${APIKEY}`;
+        
+        axios.get(url).then((response) => {
+            setData(response.data);
+            console.log(response.data);
+        });
+    };
 
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${APIKEY}`
+    useEffect(() => {
+        if (lastSearchedLocation) {
+            fetchWeatherData(lastSearchedLocation);
+        } else {
+            fetchWeatherData('Londrina'); 
+        }
+    }, [lastSearchedLocation, unit]); 
 
 
     const locationSearch = (event) => {
-
-        if(event.key == 'Enter'){
-            axios.get(url).then((response) => {
-                setData((response.data))
-                console.log(response.data)
-            })
-            setLocation('')
+        if (event.key === 'Enter') {
+            fetchWeatherData(location);
+            setLastSearchedLocation(location);
+            setLocation('');
         }
+    };
 
-    }
-    
-    return(
+    const handleTempUnitChange = (isCelsius) => {
+        setTempUnit(isCelsius ? '°C' : '°F');
+        setUnit(isCelsius ? 'metric' : 'imperial');
+        setVelocityUnit(isCelsius ? 'km/h' : 'mph');
+        setLanguage(isCelsius ? 'pt_br' : 'en');
+
+        console.log(isCelsius)
         
+    };
+
+    return (
         <div className='weather'>
-            <Toggle/>
-            <div className='toggle-Container'>
-                <div className='toggle-box'>
-
-                </div>
-
-            </div>
+            <Toggle onChange={handleTempUnitChange} />
 
             <div className='search-bar'>
                 <input 
-                value = {location} 
-                onChange = {event => setLocation(event.target.value)} 
-                onKeyDown={locationSearch} 
-                type='text' 
-                placeholder='Pesquise em um local'/>
-
+                    value={location} 
+                    onChange={event => setLocation(event.target.value)} 
+                    onKeyDown={locationSearch} 
+                    type='text' 
+                    placeholder='Pesquise em um local'
+                />
             </div>
 
             <div className='container'>
-                <div className = 'header'>
+                <div className='header'>
                     <div className='location'>
                         <p>{data.name}</p>
                     </div>
                     <div className='temperature'>
-                        {data.main ? <h1>{data.main.temp.toFixed()}{tempUnit} </h1> : null} 
+                        {data.main ? <h1>{data.main.temp.toFixed()}{tempUnit}</h1> : null}
                     </div>
-
                     <div className='description'>
-                       {data.weather? <p>{data.weather[0].description}</p> : null}
+                       {data.weather ? <p>{data.weather[0].description}</p> : null}
                     </div>
                 </div>
-
-            {data.name != undefined &&
-
-                <div className='footer'>
-                <div className='sensation'>
-                    {data.main ? data.main.feels_like.toFixed() : null} {tempUnit}
-                    <p>Sensação Térmica</p>
-                </div>
-                <div className='humidity'>
-                    {data.main ? data.main.humidity : null}%
-                    <p>Humidade</p>
-                </div>
-                <div className='wind'>
-                    {data.wind ? data.wind.speed.toFixed() : null}{velocityUnit}
-                    <p>Vento</p>
-                </div>
-
-                </div>
-            }
-
+                <DotLottieReact
+                    src="https://lottie.host/5dde1824-d9e0-45bf-90ca-45e272a75392/OyH2LOTmKa.lottie"
+                    loop
+                    autoplay
+                />
+     
+                {data.name &&
+                    <div className='footer'>
+                        <div className='sensation'>
+                            {data.main ? data.main.feels_like.toFixed() : null} {tempUnit}
+                            <p>Sensação Térmica</p>
+                        </div>
+                        <div className='humidity'>
+                            {data.main ? data.main.humidity : null}%
+                            <p>Humidade</p>
+                        </div>
+                        <div className='wind'>
+                            {data.wind ? data.wind.speed.toFixed() : null} {velocityUnit}
+                            <p>Vento</p>
+                        </div>
+                    </div>
+                }
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Weather
+export default Weather;
